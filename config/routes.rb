5f8d96a84d3494611caca03c141875543784bb41
Luís Fontes <mail.fontes@gmail.com>
2014-11-06 09:09:31 +0000

    fixe bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-#get 'stats', :to => 'stats#index'
+get 'stats', :to => 'stats#index'
 match "stats",  to: redirect("stats/all")
 get 'stats/:time_filter', :to => 'stats#index'
