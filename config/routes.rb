2c3c9d7f30191d35a420a37969d2d845d27359aa
Luís Fontes <mail.fontes@gmail.com>
2014-11-06 14:33:08 +0000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'stats', :to => 'stats#index'
-match "stats",  to: redirect("stats/current_week")
+#get 'stats', :to => 'stats#index', :time_filter => "current_week"
+match "stats",  to: redirect("/stats/current_week")
 get 'stats/:time_filter', :to => 'stats#index'
