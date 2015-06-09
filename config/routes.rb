a7cc96b2ee989615d8fcbef84ff870fafe793930
Luís Fontes <mail.fontes@gmail.com>
2015-06-09 15:56:22 +0100

    changed plugin routes to avoid conflicts & made time_filter optional

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 
 #get 'stats', :to => 'stats#index', :time_filter => "current_week"
 #match "stats",  :to => redirect("/stats/current_week")
-get 'stats/:time_filter', :to => 'stats#index'
-post 'stats/:time_filter', :to => 'stats#index'
+get 'global_stats/(:time_filter)', :to => 'stats#index'
+post 'global_stats/:time_filter', :to => 'stats#index'
